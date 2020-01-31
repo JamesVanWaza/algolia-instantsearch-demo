0a3ec18205cc5db1e059a95a2ac27c52368de3b0
JamesVanWaza <jgacuca@gmail.com>
2020-01-31 12:01:15 -0500

    Uopdated the ecommerce testing code

--- js/app-ecommerce.js
+++ js/app-ecommerce.js
@@ -1,6 +1,51 @@
+const searchClient = algoliasearch(
+  'LLER32U6UG',
+  '679d4923410abfac0793b37dccac1f3b'
+);
+
 const search = instantsearch({
-    appId: 'B1G2GM9NG0',
-    apiKey: 'aadef574be1f9252bb48d4ea09b5cfe5',
-    indexName: 'demo_ecommerce',
-    routing: true
+  indexName: 'wp_demo',
+  searchClient,
+  searchFunction(helper) {
+    if (helper.state.query) {
+      helper.search();
+    }
+  },
 });
+
+search.addWidgets([
+  instantsearch.widgets.searchBox({
+    container: '#searchbox',
+    limit: 5,
+    showMore: true,
+  }),
+
+  // This requires `tags` to be set in the `attributesForFacetings`
+  // in your Algolia index settings
+  instantsearch.widgets.refinementList({
+    container: '#tags-list',
+    attribute: 'tags',
+  }),
+
+  instantsearch.widgets.hits({
+    container: '#content',
+    templates: {
+      item: `
+      <article>
+        <a href="{{ url }}">
+          <strong>
+            {{#helpers.highlight}}
+              { "attribute": "title", "highlightedTagName": "mark" }
+            {{/helpers.highlight}}
+          </strong>
+        </a>
+        {{#content}}
+          <p>{{#helpers.snippet}}{ "attribute": "content", "highlightedTagName": "mark" }{{/helpers.snippet}}</p>
+        {{/content}}
+      </article>
+    `,
+    },
+  }),
+]);
+
+search.start();
