e3ed0aae70b21ee0041ff9748d1cca794f5bbed1
JamesVanWaza <jgacuca@gmail.com>
2020-02-10 19:28:28 -0500

    Initialized the Client - WIP

--- public/js/app-contacts.js
+++ public/js/app-contacts.js
@@ -1,13 +1,8 @@
-const algoliasearch = require('algoliasearch');
-
-const searchClient = algoliasearch(
-	'latency',
-	'6be0576ff61c053d5f9a3225e2a90f76'
-);
+const searchClient = algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76');
 
 const search = instantsearch({
-	indexName: 'contacts_search',
-	searchClient,
+  indexName: 'contacts_search',
+  searchClient,
 });
 
 const index = client.initIndex('contacts');
