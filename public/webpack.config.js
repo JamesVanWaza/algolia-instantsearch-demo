93a5e71c9e0700af5e566841ed8ba3e5a705b931
JamesVanWaza <jgacuca@gmail.com>
2020-02-15 09:14:36 -0500

    Added the webpack config file

---
+++ public/webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+
+module.exports = {
+	//	https://webpack.js.org/configuration/mode/
+	mode: 'development',
+	entry: "./src/index.js",
+//	entry: {
+//		about: './src/about.js',
+//		contact: './src/contact.js'
+//	},
+	output: {
+//		filename: "[name].bundle.js",
+		filename: "webpack-complete.js",
+//		path: path.resolve(__dirname, "public") Can change directory name
+		path: path.resolve(__dirname, "dist") 
+	},
+	// Can change the entry name
+	// entry: "./mbuzi/mbuzide.js",
+	devServer: {
+		contentBase: path.join(__dirname, "dist"),
+		port: 9000
+	},
+//	optimization: {
+////		splitChunks: {
+////			chunks: 'all'
+////		}
+////		minimizer: [new UglifyJsPlugin()]
+//	},
+	plugins: [new HtmlWebpackPlugin()],
+	module: {
+		rules: [
+			{
+				// Whenever a javascript file is found, babel should run and do not compile node_module files
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: {
+					loader: 'babel-loader', 
+					options: {
+						// Supports all the browsers
+						presets: ['@babel/preset-env', '@babel/preset-react']
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{loader: 'style-loader'},
+					{loader: 'css-loader'}
+				]
+			},
+			
+			// Start here for the URL Loader
+			{
+				test: /\.(png|jpg)$/,
+				use: [
+					{loader: 'url-loader'}
+				]
+			}
+		]
+	}
+}
