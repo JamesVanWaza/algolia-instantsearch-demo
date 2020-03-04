8df24c46776ab5b50aeb11c230207a0e11a62d8b
JamesVanWaza <jgacuca@gmail.com>
2020-03-04 17:37:13 -0500

    Changed the Header Title

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
 // import instantsearchcss from 'instantsearch.css';
 
 /** HTML in JS **/
-document.write("<h1>Hello Webpack!</h1>");
+document.write("<h1>Algolia Tutorials</h1>");
 document.write('<div class="ais-InstantSearch"><h2>InstantSearch.js e-commerce demo</h2><div class="right-panel"><div id="searchbox"></div><div id="hits"></div><div id="pagination"></div></div></div>');
 document.write("<h6>Attention:: All this code is coming from javascript - that's preety cool.</h6>");
 document.write('<div id="image"></div>');
