f82d3ef502c5fb439a4456d4b5c2f904ce77b827
JamesVanWaza <jgacuca@gmail.com>
2020-04-28 20:11:55 -0400

    The SCSS files have been added, and index.js has been moved to the js folder and renamed algolia.js

---
+++ src/js/algolia.js
@@ -0,0 +1,32 @@
+/** Stylesheet **/
+import '../scss/style.scss';
+
+/** Algolia CSS **/
+//import instantsearchcss from 'instantsearch.css';
+
+/** Algolia JS **/
+import algoliasearch from 'algoliasearch/lite';
+import instantsearch from 'instantsearch.js';
+import {
+    searchBox,
+    hits
+} from 'instantsearch.js/es/widgets';
+
+const searchClient = algoliasearch('O966BSY3LD', '6db67900bfa16b4173055f87526117a7');
+
+const search = instantsearch({
+    indexName: 'testing_manenos',
+    searchClient,
+});
+
+search.addWidgets([
+    searchBox({
+        container: "#searchbox"
+    }),
+
+    hits({
+        container: "#hits"
+    })
+]);
+
+search.start();
