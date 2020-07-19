a37e381403c25cffb721b64dbebda7bdf4dc46a5
JamesVanWaza <jgacuca@gmail.com>
2020-07-19 18:13:38 -0400

    Added AlgoliaTwoJS Code

--- src/js/algolia-two.js
+++ src/js/algolia-two.js
@@ -13,14 +13,63 @@ const search = instantsearch({
     searchClient,
 });
 
-search.addWidgets([
-    searchBox({
-        container: "#searchbox"
-    }),
 
-    hits({
-        container: "#hits"
-    })
-]);
+const index = client.initIndex('TheContacts');
+const contactsJSON = require('../json/contacts.json');
 
-search.start();
+index.saveObjects(contactsJSON, {
+    autoGenerateObjectIDIfNotExist: true
+}).then(({ objectIDs }) => {
+    console.log(objectIDs);
+});
+
+index.setSettings({
+    'customRanking': ['desc(followers)']
+}).then(() => {
+    //  done
+});
+
+index.setSettings({
+    searchableAttributes: [
+        'lastname',
+        'firstname',
+        'company',
+        'email',
+        'city',
+        'address'
+    ]
+}).then(() => {
+    // done
+});
+
+// Search for a first name
+index.search('jimmie').then(({ hits }) => {
+    console.log(hits);
+});
+
+// Search for a first name with typo
+index.search('jimie').then(({ hits }) => {
+    console.log(hits);
+});
+
+// Search for a company
+index.search('california paint').then(({ hits }) => {
+    console.log(hits);
+});
+
+// Search for a first name and a company
+index.search('jimmie paint').then(({ hits }) => {
+    console.log(hits);
+});
+
+// search.addWidgets([
+//     searchBox({
+//         container: "#searchbox"
+//     }),
+
+//     hits({
+//         container: "#hits"
+//     })
+// ]);
+
+// search.start();
