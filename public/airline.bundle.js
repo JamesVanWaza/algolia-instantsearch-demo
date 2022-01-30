acfc5f41744fac8efef2467fbfe4c416a330a163
JamesVanWaza <jgacuca@gmail.com>
2022-01-30 10:52:43 -0500

    NPM run build

--- public/airline.bundle.js
+++ public/airline.bundle.js
@@ -165,7 +165,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var algo
 /******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
 /******/ 					installedChunks[chunkId][0]();
 /******/ 				}
-/******/ 				installedChunks[chunkIds[i]] = 0;
+/******/ 				installedChunks[chunkId] = 0;
 /******/ 			}
 /******/ 			return __webpack_require__.O(result);
 /******/ 		}
