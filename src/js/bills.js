0777fe539777d69b99be3f57c758d6f944927956
JamesVanWaza <jgacuca@gmail.com>
2025-10-01 10:34:49 -0400

    Updated the id to match the bills page

---
+++ src/js/bills.js
@@ -0,0 +1,47 @@
+/** InstantSearch.JS */
+import { liteClient as algoliasearch } from 'algoliasearch/lite';
+import instantsearch from 'instantsearch.js';
+
+/** InstantsearchJS Widgets */
+import { searchBox, hits, refinementList, breadcrumb, stats, sortBy, poweredBy } from 'instantsearch.js/es/widgets';
+
+const searchClient = algoliasearch('N5OLC87R9A', '4ec613d58d623dba86b7fa0ca70cb116');
+
+const search = instantsearch({
+    indexName: 'TheBills',
+    searchClient,
+});
+
+search.addWidgets([
+    searchBox({
+        container: '#bills',
+        placeholder: 'Search for bills',
+        autofocus: true
+    }),
+
+    hits({
+        container: '#hits',
+        templates: {
+            item: `
+                <article>
+                    <h1>{{#helpers.highlight}}{ "attribute": "make" }{{/helpers.highlight}}</h1>
+                    <img>{{#helpers.highlight}}{ "attribute": "image" }{{/helpers.highlight}}</img>
+                </article`
+        }
+    }),
+
+    stats({
+        container: '#stats'
+    }),
+
+    poweredBy({
+        container: '#powered-by'
+    }),
+
+    refinementList({
+        container: '#refinement-list',
+        attribute: 'alliance',
+    })
+]);
+
+search.start();
